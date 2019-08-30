b4f2fc30a22389c46f7bce6ad9d138ed21b50264
Ayaz Ahmed <ayez_ahmed@hotmail.com>
2019-08-30 20:55:10 +0500

    updated main.rb file

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
-require "string_colors.rb"
-require "words_list.rb"
-require "hangman.rb"
+require_relative "string_colors.rb"
+require_relative "words_list.rb"
+require_relative "hangman.rb"
 
 
 play_game = true
 while play_game
-  hangman = Hangman.new(words.sample)
+  hangman = Hangman.new($words.sample)
   hangman.start_game
   play_game = hangman.replay_game?
 end
